2d949c4a33e8bc72e47ce07a31f9460b1c3cc216
haithanh03 <nguyenhaithanh1201@gmail.com>
2022-10-26 16:20:04 +0700

    baitap

---
+++ Bai1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Bai1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("*     *    *    * * * *  * * * *  *   *\r\n"
+				+ "|     |   / \\   |      * |      *  \\ /\r\n"
+				+ "*-----*  *---*  * * * *  * * * *    *\r\n"
+				+ "|     | /     \\ |        |          |\r\n"
+				+ "*     **       **        *          *\r\n"
+				+ "\r\n"
+				+ "*    *  ****** *       *       *\r\n"
+				+ "| \\  |  |       \\     / \\     /\r\n"
+				+ "*  * *  ******   *   *   *   *\r\n"
+				+ "|   \\|  |         \\ /     \\ /\r\n"
+				+ "*    *  ******     *       *\r\n"
+				+ "\r\n"
+				+ "*   *  ******     *     * * * * \r\n"
+				+ " \\ /   |         / \\    |      *\r\n"
+				+ "  *    ******   *---*   * * * * \r\n"
+				+ "  |    |       /     \\  |    \\\r\n"
+				+ "  *    ****** *       * *     *");
+	}
+
+}
